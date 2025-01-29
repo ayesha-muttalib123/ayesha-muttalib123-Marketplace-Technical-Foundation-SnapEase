cf0ab4c9154f00cfa1fcbf1ce654698d155511f2
ayesha-muttalib123 <ayeshamutallib007@gmail.com>
2025-01-30 03:08:38 +0500

    updated

--- Day-3Api-Integration/studio-template4-ecommerce-website/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import ProductSchema from './product'
-
-// Export the schemaTypes array (not just the schema object)
-export const schemaTypes: SchemaTypeDefinition[] = [ProductSchema]
